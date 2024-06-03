cf83ae214bd8dbb19948b4d0337a5f65d9cc1f9c
Clément Degat <clement.degat@gls-france.com>
2024-06-03 16:08:06 +0200

    initial commit for API clients

---
+++ API_Clients/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+class CustomerBase(BaseModel):
+    name: str
+    email: str
+
+class CustomerCreate(CustomerBase):
+    pass
+
+class CustomerUpdate(CustomerBase):
+    pass
+
+class Customer(CustomerBase):
+    id: int
+
+    class Config:
+        orm_mode = True
